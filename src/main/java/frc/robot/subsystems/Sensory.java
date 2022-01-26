91815ad125b6c213da7e7acfc9f110833ef9d70a
Aragorn Wang <wangaragorn@gmail.com>
2022-01-25 20:57:50 -0700

    ff

--- src/main/java/frc/robot/subsystems/Sensory.java
+++ src/main/java/frc/robot/subsystems/Sensory.java
@@ -1,29 +1,53 @@
 package frc.robot.subsystems;
 
 
-// import com.analog.adis16448.frc.ADIS16448_IMU;
-
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Sensory extends SubsystemBase {
-	// private static final ADIS16448_IMU imu = new ADIS1644s8_IMU();
-	DigitalInput limitSwitch = new DigitalInput(0);
-	DigitalInput limitSwitch2 = new DigitalInput(1);
+	// ----------------------------------------------------------
+	// Resources
+	private final ADIS16448_IMU imu = new ADIS16448_IMU();
 
-	Counter counter = new Counter(limitSwitch);
+	// use .get() to get back 0 or 1 limit switch output
+	private final DigitalInput limitSwitch1 = new DigitalInput(0);
+	private final DigitalInput limitSwitch2 = new DigitalInput(1);
 
-	
+	private int counter = 0;
+
+	// ----------------------------------------------------------
+	// Constructor and actions
+
 	public Sensory() {
 
 	}
 
+	// ----------------------------------------------------------
+	// Scheduler functions
+
 	@Override
 	public void periodic() {
-		SmartDashboard.putBoolean("Limit switch value", limitSwitch.get());
-		SmartDashboard.putBoolean("Limit switch value2", limitSwitch2.get());
+		// Just so we know this periodic function is running
+		SmartDashboard.putNumber("COUNTER", counter);
+
+		SmartDashboard.putBoolean("Limit Switch 1", limitSwitch1.get());
+		SmartDashboard.putBoolean("Limit Switch 2", limitSwitch2.get());
+
+		SmartDashboard.putNumber("IMU Angle", imu.getAngle());
+
+		// SmartDashboard.putNumber("Temperature from IMU", imu.getTemperature());
+			
+		// SmartDashboard.putNumber("Accel X", imu.getAccelX());
+		// SmartDashboard.putNumber("Accel Y", imu.getAccelY());
+		// SmartDashboard.putNumber("Accel Z", imu.getAccelZ());
+
+		// SmartDashboard.putNumber("Gyro X", imu.getGyroAngleX());
+		// SmartDashboard.putNumber("Gyro Y", imu.getGyroAngleY());
+		// SmartDashboard.putNumber("Gyro Z", imu.getGyroAngleZ());
+
+		counter++;
 	}
 }
