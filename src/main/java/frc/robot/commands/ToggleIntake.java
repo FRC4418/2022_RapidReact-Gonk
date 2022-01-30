17e7bc25035352f64988344b74ebbba49dd1892b
Aragorn Wang <wangaragorn@gmail.com>
2022-01-29 18:22:40 -0700

    finished ToggleIntake cmd by adding a timer
    timer delays the intake roller motor a few seconds before shutting down after being triggered by the whisker sensor

--- src/main/java/frc/robot/commands/ToggleIntake.java
+++ src/main/java/frc/robot/commands/ToggleIntake.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
@@ -10,10 +11,19 @@ import frc.robot.subsystems.Intake;
 
 public class ToggleIntake extends CommandBase {
 	// ----------------------------------------------------------
+	// Constants
+
+	private final double INTAKE_DELAY_TIME = 2.d;
+	
+	// ----------------------------------------------------------
 	// Resources
 
 	private final Intake it;
+
 	private final DigitalInput whiskerSensor = new DigitalInput(2);
+
+	// delay to keep the intake running for a few seconds, even after we trip the whisker sensor
+	private final Timer postWhiskerSensorDelayTimer = new Timer();
 
 	// ----------------------------------------------------------
 	// Constructor
@@ -44,7 +54,16 @@ public class ToggleIntake extends CommandBase {
 	// Returns true when the command should end.
 	@Override
 	public boolean isFinished() {
-		// TODO: Figure out adding 1-2 sec (or smth) delay before stopping intake
-		return whiskerSensor.get();	// when whisker sensor is tripped, stop running the intake
+		if (whiskerSensor.get() == true) {
+			postWhiskerSensorDelayTimer.start();
+		}
+
+		if (postWhiskerSensorDelayTimer.hasElapsed(INTAKE_DELAY_TIME)) {
+			postWhiskerSensorDelayTimer.stop();
+			postWhiskerSensorDelayTimer.reset();
+			return true;
+		}
+
+		return false;
 	}
 }
--- src/main/java/frc/robot/subsystems/Sensory.java
+++ src/main/java/frc/robot/subsystems/Sensory.java
@@ -3,20 +3,14 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Sensory extends SubsystemBase {
 	// ----------------------------------------------------------
 	// Resources
+
 	private final ADIS16448_IMU imu = new ADIS16448_IMU();
-
-	// use .get() to get back 0 or 1 limit switch output
-	private final DigitalInput limitSwitch1 = new DigitalInput(0);
-	private final DigitalInput limitSwitch2 = new DigitalInput(1);
-
-	private int counter = 0;
 
 	// ----------------------------------------------------------
 	// Constructor and actions
@@ -30,12 +24,6 @@ public class Sensory extends SubsystemBase {
 
 	@Override
 	public void periodic() {
-		// Just so we know this periodic function is running
-		SmartDashboard.putNumber("COUNTER", counter);
-
-		SmartDashboard.putBoolean("Limit Switch 1", limitSwitch1.get());
-		SmartDashboard.putBoolean("Limit Switch 2", limitSwitch2.get());
-
 		SmartDashboard.putNumber("IMU Angle", imu.getAngle());
 
 		// SmartDashboard.putNumber("Temperature from IMU", imu.getTemperature());
@@ -47,7 +35,5 @@ public class Sensory extends SubsystemBase {
 		// SmartDashboard.putNumber("Gyro X", imu.getGyroAngleX());
 		// SmartDashboard.putNumber("Gyro Y", imu.getGyroAngleY());
 		// SmartDashboard.putNumber("Gyro Z", imu.getGyroAngleZ());
-
-		counter++;
 	}
 }
