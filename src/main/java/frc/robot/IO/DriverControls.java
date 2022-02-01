097bed8bebf7a7a881a21548d6efb766d0cd96a8
Aragorn Wang <wangaragorn@gmail.com>
2022-02-01 01:04:12 -0700

    dog aids

--- src/main/java/frc/robot/IO/DriverControls.java
+++ src/main/java/frc/robot/IO/DriverControls.java
@@ -3,7 +3,6 @@ package frc.robot.IO;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
 import frc.robot.RobotContainer;
