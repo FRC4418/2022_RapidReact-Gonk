9e9736735915c6c5899955c798b77b10e008e436
Aragorn Wang <wangaragorn@gmail.com>
2022-02-07 12:37:16 -0700

    imu-and-gyro-please-work

--- src/main/java/frc/robot/subsystems/Sensory.java
+++ src/main/java/frc/robot/subsystems/Sensory.java
@@ -1,9 +1,9 @@
 package frc.robot.subsystems;
 
 
-// import edu.wpi.first.wpilibj.ADIS16448_IMU;
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Sensory extends SubsystemBase {
@@ -11,7 +11,7 @@ public class Sensory extends SubsystemBase {
 	// Resources
 
 	
-	// private final ADIS16448_IMU imu = new ADIS16448_IMU();
+	private final ADIS16448_IMU imu = new ADIS16448_IMU();
 
 
 	// ----------------------------------------------------------
@@ -29,16 +29,16 @@ public class Sensory extends SubsystemBase {
 
 	@Override
 	public void periodic() {
-		// SmartDashboard.putNumber("IMU Angle", imu.getAngle());
+		SmartDashboard.putNumber("IMU Angle", imu.getAngle());
 
-		// SmartDashboard.putNumber("Temperature from IMU", imu.getTemperature());
+		SmartDashboard.putNumber("Temperature from IMU", imu.getTemperature());
 			
-		// SmartDashboard.putNumber("Accel X", imu.getAccelX());
-		// SmartDashboard.putNumber("Accel Y", imu.getAccelY());
-		// SmartDashboard.putNumber("Accel Z", imu.getAccelZ());
+		SmartDashboard.putNumber("Accel X", imu.getAccelX());
+		SmartDashboard.putNumber("Accel Y", imu.getAccelY());
+		SmartDashboard.putNumber("Accel Z", imu.getAccelZ());
 
-		// SmartDashboard.putNumber("Gyro X", imu.getGyroAngleX());
-		// SmartDashboard.putNumber("Gyro Y", imu.getGyroAngleY());
-		// SmartDashboard.putNumber("Gyro Z", imu.getGyroAngleZ());
+		SmartDashboard.putNumber("Gyro X", imu.getGyroAngleX());
+		SmartDashboard.putNumber("Gyro Y", imu.getGyroAngleY());
+		SmartDashboard.putNumber("Gyro Z", imu.getGyroAngleZ());
 	}
 }
