1bb5903bd11b37a1c218378d9f3dae8dab46e792
Aragorn Wang <wangaragorn@gmail.com>
2022-02-15 18:58:28 -0700

    use offset when getting filtered accel x and y

--- src/main/java/frc/robot/subsystems/Sensory.java
+++ src/main/java/frc/robot/subsystems/Sensory.java
@@ -15,6 +15,9 @@ public class Sensory extends SubsystemBase {
 	
 	private final ADIS16448_IMU imu = new ADIS16448_IMU(ADIS16448_IMU.IMUAxis.kZ, SPI.Port.kMXP, ADIS16448_IMU.CalibrationTime._1s);
 
+	private double filteredXAccelOffset = 0.d;
+	private double filteredYAccelOffset = 0.d;
+
 
 	// ----------------------------------------------------------
 	// Constructor and methods
@@ -23,20 +26,32 @@ public class Sensory extends SubsystemBase {
 	public Sensory() {
 		imu.setYawAxis(IMUAxis.kZ);
 	}
+	
+	public Sensory calibrateIMU() {
+		imu.calibrate();	// just filters out noise (robot must be still)
+		
+		filteredXAccelOffset = imu.getXFilteredAccelAngle();
+		filteredYAccelOffset = imu.getYFilteredAccelAngle();
+		
+		return this;
+	}
+	
+	public Sensory resetIMU() {
+		imu.reset();		// zeros out current measurements (basically sets all sensor readings at current location as the "origin")
+		return this;
+	}
 
 	// rounds to two decimals
 	private double getRounded(double input) {
 		return Math.round(input * 100.0d) / 100.0d;
 	}
 
-	public Sensory calibrateIMU() {
-		imu.calibrate();	// just filters out noise (robot must be still)
-		return this;
+	private double getXFilteredAccelAngle() {
+		return imu.getXFilteredAccelAngle() - filteredXAccelOffset;
 	}
-
-	public Sensory resetIMU() {
-		imu.reset();		// zeros out current measurements (basically sets all sensor readings at current location as the "origin")
-		return this;
+	
+	private double getYFilteredAccelAngle() {
+		return imu.getYFilteredAccelAngle() - filteredYAccelOffset;
 	}
 
 
@@ -49,8 +64,8 @@ public class Sensory extends SubsystemBase {
 		SmartDashboard.putNumber("Yaw Axis", getRounded(imu.getAngle()));
 		SmartDashboard.putNumber("Temperature from IMU", getRounded(imu.getTemperature()));
 			
-		SmartDashboard.putNumber("Filtered Accel X", getRounded(imu.getXFilteredAccelAngle()));
-		SmartDashboard.putNumber("Filtered Accel Y", getRounded(imu.getYFilteredAccelAngle()));
+		SmartDashboard.putNumber("Filtered Accel X", getRounded(getXFilteredAccelAngle()));
+		SmartDashboard.putNumber("Filtered Accel Y", getRounded(getYFilteredAccelAngle()));
 
 		SmartDashboard.putNumber("Gyro X", getRounded(imu.getGyroAngleX()));
 		SmartDashboard.putNumber("Gyro Y", getRounded(imu.getGyroAngleY()));
