ad01914e2baef7e62664f0ee030a108249c511e0
Aragorn Wang <wangaragorn@gmail.com>
2022-02-19 15:56:09 -0700

    Lights still don't work -- might need to set up a register address on arduino or just use java's i2c stuff

--- src/main/java/frc/robot/subsystems/Lights.java
+++ src/main/java/frc/robot/subsystems/Lights.java
@@ -1,22 +1,41 @@
 package frc.robot.subsystems;
 
 
+import java.math.BigInteger;
+
 import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.shuffleboard.SendableCameraWrapper;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Lights extends SubsystemBase {
+	// ----------------------------------------------------------
+	// Resources
+
+
 	private final int I2C_DEVICE_ADDRESS = 0x44;
 	private final I2C arduino = new I2C(Port.kOnboard, I2C_DEVICE_ADDRESS);
 	
 
+	// ----------------------------------------------------------
+	// Constructor
+
+
 	public Lights() {
-		
+
 	}
+
+
+	// ----------------------------------------------------------
+	// Scheduler methods
+
 
 	@Override
 	public void periodic() {
-		arduino.write(0, 4);
+		byte[] sendData = (BigInteger.valueOf(4)).toByteArray();
+		// arduino.writeBulk(sendData);
+		arduino.write(2, 4);
 	}
 }
