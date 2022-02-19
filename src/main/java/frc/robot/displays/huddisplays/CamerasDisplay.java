b67030cc420c8d2d55e9d34493a37060740d8e7a
Aragorn Wang <wangaragorn@gmail.com>
2022-02-18 18:55:19 -0700

    New CameraDisplay HUDDisplay created

---
+++ src/main/java/frc/robot/displays/huddisplays/CamerasDisplay.java
@@ -0,0 +1,53 @@
+package frc.robot.displays.huddisplays;
+
+
+import java.util.Map;
+
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+import frc.robot.displays.Display;
+import frc.robot.subsystems.Autonomous.AutonomousRoutine;
+
+
+public class CamerasDisplay extends HUDDisplay {
+    // ----------------------------------------------------------
+    // Resources
+
+    
+
+    // ----------------------------------------------------------
+    // Constructor (initializes the display the same time)
+    
+    public CamerasDisplay(int column, int row) {
+		super(column, row);
+    }
+
+	@Override
+	protected Display createEntriesArray() {
+		
+		return this;
+	}
+
+	@Override
+	protected Display createDisplayAt(int column, int row) {
+		{ var camerasLayout = hudTab
+			.getLayout("Cameras", BuiltInLayouts.kGrid)
+			.withProperties(Map.of("Number of columns", 1, "Number of rows", 1, "Label position", "HIDDEN"))
+			.withPosition(column, row)
+			.withSize(2, 1);
+			
+			// camerasLayout
+			// 	.add("Autonomous Routine", autoRoutineChooser)
+			// 	.withWidget(BuiltInWidgets.kComboBoxChooser);
+		}
+		return this;
+	}
+
+	@Override
+	public Display addEntryListeners() {
+		
+		return this;
+	}
+}
