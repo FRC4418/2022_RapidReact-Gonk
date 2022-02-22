edaad0d4e7563265fd284c1ec028eb15e0035e88
Aragorn Wang <wangaragorn@gmail.com>
2022-02-21 20:52:55 -0700

    UNFINISHED first draft of putting auto routines in their own commands

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -66,8 +66,8 @@ public class Robot extends TimedRobot {
 			.listenForJoystickDevices();
 		
 		if (RobotContainer.enableDiagnostics) {
-			robotContainer
-				.printJoystickValues();
+			// robotContainer
+			// 	.printJoystickValues();
 		}
 	}
 
--- src/main/java/frc/robot/subsystems/Autonomous.java
+++ src/main/java/frc/robot/subsystems/Autonomous.java
@@ -11,8 +11,26 @@ public class Autonomous extends SubsystemBase {
 
 	// TODO: P2 Figure out all the autonomous strategy options we should have
 	public enum AutonomousRoutine {
-		DRIVE_STRAIGHT_BACKWARDS,
-		DRIVE_STRAIGHT_TO_LOW_HUB
+		// 'PICKUP_CARGO' just directly runs intake to collect the cargo that's assumed to be right behind us
+		// 'RETRIEVE_CARGO' uses vision and the IMU to autonomously find the closest cargo and collect it 
+		// LT = leave tarmac
+		// LH = score low hub
+		// PC = pickup cargo
+		// RC = retrieve cargo
+		LEAVE_TARMAC									(1),	// LT
+		SCORE_LH_AND_LEAVE_TARMAC						(2),	// LH LT
+		SCORE_LH_AND_PICKUP_CARGO_AND_LEAVE_TARMAC		(3),	// LH PC LT
+		SCORE_LH_AND_RETRIEVE_CARGO_AND_LEAVE_TARMAC	(4);	// LH RC LT
+
+		private final int value;
+
+		private AutonomousRoutine(int value) {
+			this.value = value;
+		}
+
+		public int getValue() {
+			return value;
+		}
 	}
 
 
