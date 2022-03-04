4b2976cb5e8b0fc04e783befac2608084a268318
Aragorn Wang <wangaragorn@gmail.com>
2022-03-03 20:54:41 -0700

    BLANK New climber subsystem

---
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Climber extends SubsystemBase {
+	// ----------------------------------------------------------
+	// Resources
+
+
+	
+
+
+	// ----------------------------------------------------------
+	// Constructor
+
+
+	public Climber() {
+
+	}
+
+
+	// ----------------------------------------------------------
+	// Scheduler methods
+
+	@Override
+	public void periodic() {
+
+	}
+
+
+	// ----------------------------------------------------------
+	// 
+}
