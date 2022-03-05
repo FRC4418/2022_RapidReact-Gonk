b317d660367d0c1b0d97be962ab99fd2f240dc66
Aragorn Wang <wangaragorn@gmail.com>
2022-03-04 23:43:35 -0700

    UNTESTED Gyro-supported DriveStraightForDistance

--- src/main/java/frc/robot/commands/drivetrain/DriveStraight.java
+++ src/main/java/frc/robot/commands/drivetrain/DriveStraight.java
@@ -8,20 +8,31 @@ import frc.robot.subsystems.Drivetrain;
 
 public class DriveStraight extends CommandBase {
 	// ----------------------------------------------------------
+	// Public constants
+
+	public enum DriveStraightDirection {
+		FORWARDS,
+		BACKWARDS
+	}
+
+	// ----------------------------------------------------------
 	// Private constants
 
-	// in meters per second
-	private final double MOTOR_SPEED = 0.7;
+	protected final double kMotorMPS = 3.;
+	protected final double kP = 0.03;
 
 	// ----------------------------------------------------------
 	// Resources
 
-	private final Drivetrain m_drivetrain;
+	protected final Drivetrain m_drivetrain;
+
+	protected final DriveStraightDirection m_direction;
 
 	// ----------------------------------------------------------
 	// Constructor
 
-	public DriveStraight(Drivetrain drivetrain) {
+	public DriveStraight(Drivetrain drivetrain, DriveStraightDirection direction) {
+		m_direction = direction;
 		m_drivetrain = drivetrain;
 		
 		addRequirements(drivetrain);
@@ -31,24 +42,39 @@ public class DriveStraight extends CommandBase {
 	// Scheduler methods
 
 	@Override
-	public boolean runsWhenDisabled() {
-		return false;
-	}
-
-	@Override
 	public void initialize() {
-		m_drivetrain.disableOpenLoopRamp();
+		m_drivetrain
+			.disableOpenLoopRamp()
+			.resetIMU()
+			.resetEncoders();
+
+		// we do this so that using the tank drive functions with positive speeds still works (only if we're driving backwards)
+		if (m_direction == DriveStraightDirection.BACKWARDS) {
+			m_drivetrain.reverseDrivetrain();
+		}
 	}
 
 	@Override
 	public void execute() {
-		m_drivetrain.tankDrive(MOTOR_SPEED, MOTOR_SPEED);
+		var error = m_drivetrain.getHeading();
+
+		var leftTankMPS = kMotorMPS + kP * error;
+		var rightTankMPS = kMotorMPS - kP * error;
+
+		m_drivetrain
+			.setLeftMPS(leftTankMPS)
+			.setRightMPS(rightTankMPS);
 	}
 
 	@Override
 	public void end(boolean interrupted) {
 		m_drivetrain.stopDrive();
 		m_drivetrain.useJoystickDrivingOpenLoopRamp();
+
+		// undo the drivetrain reversal we did in the initialization (only if we're driving backwards)
+		if (m_direction == DriveStraightDirection.BACKWARDS) {
+			m_drivetrain.reverseDrivetrain();
+		}
 	}
 
 	@Override
