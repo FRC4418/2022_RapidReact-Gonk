b80d2c6a928c8d5f96de58d0422a2bd3bfd01c67
Aragorn Wang <wangaragorn@gmail.com>
2022-03-04 20:57:25 -0700

    ff

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
@@ -9,7 +10,10 @@ public class Climber extends SubsystemBase {
 	// Resources
 
 
-	
+	private final Servo m_leftPinServo = new Servo(0);
+	private final Servo m_rightPinServo = new Servo(2);
+
+	private final double m_pintOutAngle = 30.;
 
 
 	// ----------------------------------------------------------
