4e50d1630523ac47089a07efce9658d05e83fd57
Aragorn Wang <wangaragorn@gmail.com>
2022-03-22 22:51:17 -0600

    ff

--- src/main/java/frc/robot/displays/autonomous/PremadeAutoRoutineDisplay.java
+++ src/main/java/frc/robot/displays/autonomous/PremadeAutoRoutineDisplay.java
@@ -77,7 +77,7 @@ public class PremadeAutoRoutineDisplay extends AutonomousDisplay {
 				.withProperties(Map.of("Number of columns", 1, "Number of rows", 5, "Label position", "TOP"));
 
 				// setting default options for sendable choosers also adds the label-value pair as an option
-				autoRoutineChooser.setDefaultOption("Wait LH PC Wait LH", AutonomousRoutine.WAIT_AND_SCORE_LH_AND_PICKUP_CARGO_AND_WAIT_AND_SCORE_LH);
+				autoRoutineChooser.setDefaultOption("Wait LH PC Wait LH", AutonomousRoutine.Wait_LH_PC_Wait_LH_LT);
 				autoRoutineChooser.addOption("Wait LT", AutonomousRoutine.WAIT_AND_LEAVE_TARMAC);
 				autoRoutineChooser.addOption("Wait LH LT", AutonomousRoutine.WAIT_SCORE_LH_AND_LEAVE_TARMAC);
 				autoRoutineChooser.addOption("LH Wait LT", AutonomousRoutine.SCORE_LH_AND_WAIT_AND_LEAVE_TARMAC);
